bf4d1f84b0edae39852d0c5fb8d8c7d49342f36c
Rama <ramiroquesadaperez@gmail.com>
2022-12-11 12:03:02 -0300

    almost finished

--- app.js
+++ app.js
@@ -1,61 +1,106 @@
 //Elementos del DOM
+const cellElements = document.querySelectorAll("[data-cell]");
+const X_CLASS = "x";
+const CIRCLE_CLASS = "circle";
+const WINNING_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
-const X_CLASS ='x';
-const CIRCLE_CLASS='circle';
+let winningMessageTextElement = document.querySelector(
+  "[data-winning-message-text]"
+);
+let winningMessageElement = document.getElementById("winningMessage");
 
-const cellElements = document.querySelectorAll('[data-cell]');
+let restartButton = document.getElementById("restartButton");
+
 let circleTurn;
 
+startGame();
+
+restartButton.addEventListener("click", startGame);
 
 //Event listener para las celdas
-cellElements.forEach(cell=>{
-    cell.addEventListener('click', handleClick, { once:true })
-})
 
+function startGame() {
+  circleTurn = false;
+  cellElements.forEach((cell) => {
+    cell.innerHTML = "";
+    cell.classList.remove(X_CLASS);
+    cell.classList.remove(CIRCLE_CLASS);
+    cell.removeEventListener("click", handleClick);
 
-function handleClick(e){
+    cell.addEventListener("click", handleClick, { once: true });
+  });
 
-    console.log(e.target)
-    const cell = e.target;
-    const currentCLass = circleTurn ? CIRCLE_CLASS : X_CLASS;
+  winningMessageElement.classList.remove("show");
+}
 
-    placeMark(cell, currentCLass)
-    // Poner marca
+function handleClick(e) {
+  const cell = e.target;
+  const currentCLass = circleTurn ? CIRCLE_CLASS : X_CLASS;
 
-    //Chequear victoria
+  // Poner marca
+  placeMark(cell, currentCLass);
+  //Chequear victoria
 
-    //Chequear empate
-
-    //Switch Turns
+  if (checkWin(currentCLass)) {
+    endGame(false);
+  } else if (isDraw()) {
+    endGame(true);
+  } else {
     swapTurns();
-       
-}
+  }
 
-
-
-
-let placeMark =(cell, currentCLass)=>{
-    let newImg = document.createElement('img');   
-    newImg.classList.add('mark');
-    
-    console.log(newImg)
-    
-
-    if (currentCLass == CIRCLE_CLASS){
-        newImg.setAttribute('src', '/imgs/circle.png')
-        cell.append(newImg)
-        
-
-    }
-    else {
-        newImg.setAttribute('src', '/imgs/candy.png')
-        cell.append(newImg)
+  //Chequear empate
+  function endGame(draw) {
+    if (draw) {
+      winningMessageTextElement.innerText = `Empate!`;
+    } else {
+      winningMessageTextElement.innerText = `${circleTurn ? "O" : "X"} gana!`;
     }
 
+    winningMessageElement.classList.add("show");
+  }
 
+  function isDraw() {
+    return [...cellElements].every((cell) => {
+      return (
+        cell.classList.contains(X_CLASS) ||
+        cell.classList.contains(CIRCLE_CLASS)
+      );
+    });
+  }
 }
 
+let placeMark = (cell, currentCLass) => {
+  let newImg = document.createElement("img");
+  newImg.classList.add("mark");
+  cell.classList.add(currentCLass);
 
-let swapTurns = ()=>{
-    circleTurn  = !circleTurn
-}
+  if (currentCLass == CIRCLE_CLASS) {
+    newImg.setAttribute("src", "/imgs/circle.png");
+    cell.append(newImg);
+  } else {
+    newImg.setAttribute("src", "/imgs/candy.png");
+    cell.append(newImg);
+  }
+};
+
+let swapTurns = () => {
+  circleTurn = !circleTurn;
+};
+
+let checkWin = (currentCLass) => {
+  return WINNING_COMBINATIONS.some((combination) => {
+    return combination.every((index) => {
+      return cellElements[index].classList.contains(currentCLass);
+    });
+  });
+};
